5ce821fa7f66eca5e90a08e87104cdfcb745a995
Yohan Youmbi <31018343+iEatFishFingers@users.noreply.github.com>
2022-02-10 17:01:38 +0000

    tracking
    pokemons load onto the cards and set the stats for each pokemon

---
+++ Assets/Pokemons/Script/PokemonBase.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PokemonBase : MonoBehaviour
+{
+    public string name;
+    public float baseHP;
+    public float currHP;
+    public float baseMP;
+    public float currMP;
+
+    
+    public enum Type
+    {
+
+        Grass,
+        Fire,
+        Earth,
+        Water,
+        Electric,
+        Air,
+        Fighting
+
+    }
+
+    public enum Rarity
+    {
+        Common,
+        Uncommon,
+        Rare,
+        SuperRare,
+        Legendary
+
+    }
+
+    public Type PokemonType;
+    public Rarity rarity;
+    public float baseATK;
+    public float currATK;
+    public float baseDEF;
+    public float currDEF;
+
+}
